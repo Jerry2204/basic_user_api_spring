e6ab64445ea74b2a90a5b32c16126cc2552573fb
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-10-13 22:22:21 +0700

    add UpdateUserRequest model

---
+++ src/main/java/jerryproject/springrestfulapi/model/UpdateUserRequest.java
@@ -0,0 +1,18 @@
+package jerryproject.springrestfulapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateUserRequest {
+
+    private String name;
+
+    private String password;
+
+}
