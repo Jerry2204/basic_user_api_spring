776e881142125c755451109a114bfafad30f5619
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-10-15 13:22:21 +0700

    add ContactService and method create

---
+++ src/main/java/jerryproject/springrestfulapi/service/ContactService.java
@@ -0,0 +1,44 @@
+package jerryproject.springrestfulapi.service;
+
+import jerryproject.springrestfulapi.entity.Contact;
+import jerryproject.springrestfulapi.entity.User;
+import jerryproject.springrestfulapi.model.ContactResponse;
+import jerryproject.springrestfulapi.model.CreateContactRequest;
+import jerryproject.springrestfulapi.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private ValidationService validationService;
+
+    @Transactional
+    public ContactResponse create(User user, CreateContactRequest request) {
+        validationService.validate(request);
+
+        Contact contact = new Contact();
+        contact.setId(UUID.randomUUID().toString());
+        contact.setFirstName(request.getFirstName());
+        contact.setLastName(request.getLastName());
+        contact.setEmail(request.getEmail());
+        contact.setPhone(request.getPhone());
+        contactRepository.save(contact);
+
+        return ContactResponse.builder()
+                .id(contact.getId())
+                .firstName(contact.getFirstName())
+                .lastName(contact.getLastName())
+                .email(contact.getEmail())
+                .phone(contact.getPhone())
+                .build();
+    }
+
+}
