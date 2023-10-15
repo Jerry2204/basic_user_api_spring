307ae1551918dd846a2cabc4cd3675e4f6375a76
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-10-15 13:10:06 +0700

    add ContactRepository

---
+++ src/main/java/jerryproject/springrestfulapi/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package jerryproject.springrestfulapi.repository;
+
+import jerryproject.springrestfulapi.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, String> {
+}
