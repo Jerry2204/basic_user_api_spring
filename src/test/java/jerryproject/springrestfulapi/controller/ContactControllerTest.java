80856b0190374116ab82ac96dd206aa1f9a8f3bb
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-10-17 23:52:58 +0700

    add deleteContactNotFound unit test

--- src/test/java/jerryproject/springrestfulapi/controller/ContactControllerTest.java
+++ src/test/java/jerryproject/springrestfulapi/controller/ContactControllerTest.java
@@ -225,4 +225,24 @@ class ContactControllerTest {
             assertTrue(contactRepository.existsById(response.getData().getId()));
         });
     }
+
+    @Test
+    void deleteContactNotFound() throws Exception {
+        mockMvc.perform(
+                delete("/api/contacts/1927192")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-API-TOKEN", "test")
+        ).andExpectAll(
+                status().isNotFound()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<String>>() {
+            });
+
+            assertNotNull(response.getErrors());
+
+        });
+    }
+
+
 }
